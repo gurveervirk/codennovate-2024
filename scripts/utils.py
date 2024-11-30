48bee1319343d387c5675dbaf45a2db34789c478
gurveervirk <gurveervirk.15517@gmail.com>
2024-11-30 23:24:47 +0530

    debugging...

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,9 +8,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import time
 
-def get_driver(options, debug):
+def get_driver(options):
     return webdriver.Chrome(
         service=Service(
             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
@@ -26,8 +27,10 @@ def scrape_urls(base_url, is_streamlit=False):
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
-        driver = get_driver(options=options, debug=debug)
+        ua = UserAgent()
+        user_agent = ua.random
+        options.add_argument(f'--user-agent={user_agent}')
+        driver = get_driver(options=options)
     else:
         driver = webdriver.Chrome()
     driver.get(base_url)
