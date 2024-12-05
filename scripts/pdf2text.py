7f279b006d92c4252880e8226d9e503c15092e63
gurveervirk <gurveervirk.15517@gmail.com>
2024-12-05 11:15:24 +0530

    latest code

--- scripts/pdf2text.py
+++ scripts/pdf2text.py
@@ -13,65 +13,42 @@ from PIL import Image
 import pytesseract
 import fitz  # PyMuPDF for image extraction
 import io
-def extract_text_and_images_with_ocr(pdf_path):
-    # Initialize reader for text and PyMuPDF for images
-    pdf_reader = PdfReader(pdf_path)
-    pdf_doc = fitz.open(pdf_path)
-    combined_content = []
 
-    for page_number, (pdf_page, fitz_page) in enumerate(zip(pdf_reader.pages, pdf_doc), start=1):
-        print(f"Processing page {page_number}...")
-        
-        # Extract text content
-        text_content = pdf_page.extract_text().strip() if pdf_page.extract_text() else ""
-
-        # Extract images and apply OCR
-        image_texts = []
-        angle=fitz_page.rotation
-        for img_index, img in enumerate(fitz_page.get_images(full=True)):
-            xref = img[0]
-            base_image = pdf_doc.extract_image(xref)
-            image_bytes = base_image["image"]
-            image = Image.open(io.BytesIO(image_bytes))
-            image = image.rotate(-angle, Image.NEAREST, expand = 1)
-            if angle!=0: print(f'Page was rotated by {angle}°. [FIXED]')
-            # image.show()
-            
-            # Apply OCR on the image
-            ocr_text = pytesseract.image_to_string(image)
-            image_texts.append(ocr_text)
-
-        # Combine text content and OCR results
-        combined_page_content = f"Page {page_number}:\n{text_content}\n" + "\n".join(image_texts)
-        combined_content.append(combined_page_content)
-
-    # Combine all pages
-    print(combined_content)
-    return "\n\n".join(combined_content)
-
-
-# Example usage
-import io
-
-pdf_path = ""
-result = extract_text_and_images_with_ocr(pdf_path)
-
-# Save to a file or print
-with open("", "w", encoding="utf-8") as f:
-    f.write(result)
-
-print("Mixed-content extraction complete!")
 
 class MyFileReader(BaseReader):
     def load_data(self, file, extra_info=None):
         # load_data returns a list of Document objects
-        print(file)
-        text = extract_text_and_images_with_ocr(file)
-        return [Document(text=text, extra_info=extra_info or {})]
+        pdf_reader = PdfReader(file)
+        pdf_doc = fitz.open(file)
+        combined_content = []
+        file = str(file)
+        for page_number, (pdf_page, fitz_page) in enumerate(zip(pdf_reader.pages, pdf_doc), start=1):
+            
+            # Extract text content
+            text_content = pdf_page.extract_text().strip() if pdf_page.extract_text() else ""
+            file_name = file.split('/')[-1]
+            # Extract images and apply OCR
+            image_texts = []
+            angle=fitz_page.rotation
+            for img_index, img in enumerate(fitz_page.get_images(full=True)):
+                xref = img[0]
+                base_image = pdf_doc.extract_image(xref)
+                image_bytes = base_image["image"]
+                image = Image.open(io.BytesIO(image_bytes))
+                image = image.rotate(-angle, Image.NEAREST, expand = 1)
+                image = image.convert('RGB')
+                # image.show()
+                
+                # Apply OCR on the image
+                ocr_text = pytesseract.image_to_string(image)
+                image_texts.append(ocr_text)
 
+            # Combine text content and OCR results
+            combined_page_content = f"{text_content}\n" + "\n".join(image_texts)
+            metadata={"page_label":page_number,"file_name":file_name}
+            if extra_info is not None:
+                metadata.update(extra_info)
+            combined_content.append(Document(text=combined_page_content,metadata=metadata))
 
-reader = SimpleDirectoryReader(
-    input_dir="./data", file_extractor={".pdf": MyFileReader()}
-)
-
-documents = reader.load_data()
+        # Combine all pages
+        return combined_content
--- scripts/streamlit_app.py
+++ scripts/streamlit_app.py
@@ -2,13 +2,81 @@ import streamlit as st
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.embeddings.gemini import GeminiEmbedding
+# from llama_index.core.postprocessor import FixedRecencyPostprocessor
 from llama_index.vector_stores.neo4jvector import Neo4jVectorStore
 from llama_index.llms.gemini import Gemini
 from streamlit_cookies_controller import CookieController
+from llama_index.core.extractors import (
+    SummaryExtractor,
+    QuestionsAnsweredExtractor
+)
+from typing import List, Optional
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+import numpy as np
+from llama_index.core.base.embeddings.base import BaseEmbedding
+from llama_index.core.bridge.pydantic import Field, SerializeAsAny
+from llama_index.core.postprocessor.types import BaseNodePostprocessor
+from llama_index.core.schema import MetadataMode, NodeWithScore, QueryBundle
+from llama_index.core.settings import Settings
+from llama_index.core.vector_stores import MetadataFilters, ExactMatchFilter, MetadataFilter, FilterOperator
+
+class FixedRecencyPostprocessor(BaseNodePostprocessor):
+    """Fixed Recency post-processor.
+
+    This post-processor does the following steps orders nodes by date.
+
+    Assumes the date_key corresponds to a date field in the metadata.
+    """
+
+    top_k: int = 1
+    date_key: str = "date"
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "FixedRecencyPostprocessor"
+
+    def _postprocess_nodes(
+        self,
+        nodes: List[NodeWithScore],
+        query_bundle: Optional[QueryBundle] = None,
+    ) -> List[NodeWithScore]:
+        """Postprocess nodes."""
+        try:
+            import pandas as pd
+        except ImportError:
+            raise ImportError(
+                "pandas is required for this function. Please install it with pip install pandas."
+            )
+
+        if query_bundle is None:
+            raise ValueError("Missing query bundle in extra info.")
+
+        # sort nodes by date
+        node_dates = pd.to_datetime(
+            [node.node.metadata[self.date_key] for node in nodes],
+            dayfirst=True,
+        )
+        logging.info(f"Node dates: {node_dates}")
+        sorted_node_idxs = np.flip(node_dates.argsort())
+        sorted_nodes = [nodes[idx] for idx in sorted_node_idxs]
+
+        return sorted_nodes[: self.top_k]
+
+
 import os
 import utils  # Importing the utilities script for link processing
+from pdf2text import MyFileReader  # Importing the custom PDF reader
 import requests
 import shutil  # For moving files from 'new' to 'data'
+
+# Global Variable for VectorStoreIndex
+if "index" not in st.session_state:
+    st.session_state.index = None
 
 # Initialize Cookie Controller
 controller = CookieController()
@@ -95,7 +163,7 @@ if st.button("Process Links"):
             # Initialize progress bar
             overall_progress = st.progress(0)
             total_pdfs = len(pdf_links)
-
+            downloaded = 0
             for index, pdf_url in enumerate(pdf_links):
                 pdf_name = os.path.basename(pdf_url)
                 pdf_path = os.path.join(new_dir, pdf_name)
@@ -104,6 +172,7 @@ if st.button("Process Links"):
                     response = requests.get(pdf_url, stream=True)
                     response.raise_for_status()  # Raise an error for HTTP issues
                     with open(pdf_path, "wb") as pdf_file:
+                        downloaded += 1
                         for chunk in response.iter_content(chunk_size=1024):
                             if chunk:  # Filter out keep-alive chunks
                                 pdf_file.write(chunk)
@@ -113,9 +182,20 @@ if st.button("Process Links"):
                 # Update overall progress bar
                 overall_progress.progress((index + 1) / total_pdfs)
             
-            st.success(f"Downloaded {len(pdf_links)} PDFs!")
+            st.success(f"Downloaded {downloaded} PDFs!")
 
 # Step 4: Index Documents
+Settings.embed_model = GeminiEmbedding(api_key=api_key, model_name="models/text-embedding-004")
+Settings.llm = Gemini(api_key=api_key, model="models/gemini-1.5-flash")
+if "vector_store" not in st.session_state and all([api_key, neo4j_url, neo4j_username, neo4j_password]):
+    st.session_state.vector_store = Neo4jVectorStore(
+        neo4j_username,
+        neo4j_password,
+        neo4j_url,
+        embed_dim,
+        hybrid_search=True,
+    )
+    st.session_state.index = VectorStoreIndex.from_vector_store(vector_store=st.session_state.vector_store)
 if st.button("Index Documents"):
     if not all([api_key, neo4j_url, neo4j_username, neo4j_password]):
         st.error("Please set all credentials in the sidebar.")
@@ -123,23 +203,14 @@ if st.button("Index Documents"):
         with st.spinner("Indexing documents... This may take a few minutes..."):
             try:
                 # Configure embeddings and Neo4j store
-                Settings.embed_model = GeminiEmbedding(api_key=api_key, model_name="models/text-embedding-004")
-                Settings.llm = Gemini(api_key=api_key, model="models/gemini-1.5-flash")
-                neo4j_vector = Neo4jVectorStore(
-                    neo4j_username,
-                    neo4j_password,
-                    neo4j_url,
-                    embed_dim,
-                    hybrid_search=True
-                )
-
+                # qa_extractor = QuestionsAnsweredExtractor(questions=5)
+                # summary_extractor = SummaryExtractor()  
                 def get_metadata(file_path):
                     return st.session_state.extracted_data.get(os.path.basename(file_path), {})
                 # Load documents from the 'new' directory (where PDFs were downloaded)
-                
-                documents = SimpleDirectoryReader(input_dir=new_dir,file_metadata=get_metadata).load_data()
-                storage_context = StorageContext.from_defaults(vector_store=neo4j_vector)
-                index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, show_progress=True)
+                documents = SimpleDirectoryReader(input_dir=new_dir,file_metadata=get_metadata,file_extractor={".pdf": MyFileReader()}).load_data()
+                storage_context = StorageContext.from_defaults(vector_store=st.session_state.vector_store)
+                st.session_state.index=VectorStoreIndex.from_documents(documents,  storage_context=storage_context, show_progress=True)
                 st.success("Documents indexed successfully!")
 
                 # After indexing, move the files from 'new' to the 'data' directory and clear 'new'
@@ -160,33 +231,67 @@ if st.button("Index Documents"):
 st.header("Chat with Your Documents")
 query = st.text_input("Ask a question about the documents:")
 
-if st.button("Chat"):
+if st.button("Ask"):
     if not query:
         st.error("Please enter a query.")
     else:
         try:
             # Chat engine setup
+
+# Define metadata filters
+            filters = MetadataFilters(
+                filters=[ExactMatchFilter(key="type", value="Policy Documents")]
+            )
             memory = ChatMemoryBuffer.from_defaults(token_limit=4096)
-            chat_engine = index.as_chat_engine(
+            chat_engine = st.session_state.index.as_chat_engine(
                 chat_mode="context",
                 memory=memory,
                 system_prompt=( 
-                    "You are a chatbot, able to have normal interactions, as well as use "
-                    "the context provided to return complete, detailed answers."
-                )
+                    "You are an expert in Indian trade policies. Use the policy documents as the foundation for your answers. "
+                ),
+                filters=filters
             )
-
+            not_equal_filter = MetadataFilter(
+                key="type",
+                value="Policy Documents",
+                operator=FilterOperator.NE
+            )
+            metadata_filters = MetadataFilters(
+                filters=[not_equal_filter]
+            )
             # Query the engine
             response = chat_engine.chat(query)
-
+            st.subheader("Base Answer:")
+            st.write(response.response)
+            source_nodes = response.source_nodes
+            # Define recency postprocessor
+            # recency_postprocessor = FixedRecencyPostprocessor(top_k=25, date_key="date")
+            # Configure chat engine with recency postprocessor
+            chat_engine = st.session_state.index.as_chat_engine(
+                memory=memory,
+                system_prompt=( 
+                    "You are an expert in Indian trade policies and you have used policy documents as the foundation for your answers. "
+                    " Use the context provided to update and refine the generated output provided below with the latest information and context."
+                    "Output:\n{response}"
+                ),
+                filters=metadata_filters,
+                # node_postprocessors=[recency_postprocessor]
+            )
+            # Use the chat engine
+            response = chat_engine.chat(query)
+            source_nodes.extend(response.source_nodes)
             # Display response
-            st.subheader("Answer:")
-            st.write(response.response_text)
+            st.subheader("Refined Answer:")
+            st.write(response.response)
 
             # Display relevant nodes with page numbers
             st.subheader("Relevant Nodes:")
-            for node in response.source_nodes:
-                st.write(f"- Page {node.metadata.get('page_label', 'Unknown')}: {node.content}")
+            st.write(f"Retrieved {len(source_nodes)} nodes")
+            for node in source_nodes:
+                st.write(f"     Document Name: {node.metadata.get('title', '')}")
+                st.write(f"     Page Number: {node.metadata.get('page_label', '')}")
+                st.markdown("       [Document link](%s)" % node.metadata.get("attachment", ""))
+                st.write(f"- {node.node.get_text().strip()}")
 
         except Exception as e:
             st.error(f"Error processing query: {e}")
